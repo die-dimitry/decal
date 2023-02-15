859b15559e53f30f13ca6c6d01b75e94b19abafe
die-dimitry <yunus787686@gmail.com>
2023-02-15 23:18:43 +0530

    Original

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:arcore_flutter_plugin_example/app.dart';
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart'
-    show ArCoreController;
+      show ArCoreController;
+import 'package:firebase_core/firebase_core.dart';
 
-void main() async
+
+Future<void> main() async
+
 {
+  try
+  {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+  }
+  catch (errorMsg)
+  {
+   print("Error:: " + errorMsg.toString());
+  }
+
   WidgetsFlutterBinding.ensureInitialized();
 
   print('ARCORE IS AVAILABLE?');
